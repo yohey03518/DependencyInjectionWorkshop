acd0e745d25ed8e1087e59817cf7547cc261eb6b
erwin <erwin.chang@titansoft.com.sg>
2020-01-04 09:30:10 +0800

    Complete Verify and Copy paste 91 provided function and inline

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,50 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string accountId, string inputPwd, string otp)
+        {
+            string ret;
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                var password = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+
+                ret = password;
+            }
+
+            string pwdInDb = ret;
+
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(inputPwd));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            var hashedInputPWd = hash.ToString();
+
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            if (response.IsSuccessStatusCode)
+            {
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
+
+            var currentOtp = response.Content.ReadAsAsync<string>().Result;
+            return pwdInDb == hashedInputPWd && currentOtp == otp;
+        }
     }
 }
