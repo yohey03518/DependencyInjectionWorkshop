b20d32162af87e88b033df5109dba669374a43cd
erwin <erwin.chang@titansoft.com.sg>
2020-01-04 12:09:15 +0800

    Move FailCounter to new file

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -26,25 +26,6 @@ namespace DependencyInjectionWorkshop.Models
         }
     }
 
-    public class FailCounter
-    {
-        public FailCounter()
-        {
-        }
-
-        public void Reset(string accountId, HttpClient httpClient)
-        {
-            var resetResponse = httpClient.PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
-            resetResponse.EnsureSuccessStatusCode();
-        }
-
-        public void AddFailCount(string accountId, HttpClient httpClient)
-        {
-            var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
-            addFailedCountResponse.EnsureSuccessStatusCode();
-        }
-    }
-
     public class AuthenticationService
     {
         private ProfileDao _profileDao;
---
+++ DependencyInjectionWorkshop/Models/FailCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+
+namespace DependencyInjectionWorkshop.Models
+{
+    public class FailCounter
+    {
+        public FailCounter()
+        {
+        }
+
+        public void Reset(string accountId, HttpClient httpClient)
+        {
+            var resetResponse = httpClient.PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
+            resetResponse.EnsureSuccessStatusCode();
+        }
+
+        public void AddFailCount(string accountId, HttpClient httpClient)
+        {
+            var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
+            addFailedCountResponse.EnsureSuccessStatusCode();
+        }
+    }
+}
