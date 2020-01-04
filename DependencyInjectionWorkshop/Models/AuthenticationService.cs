ed7f00f249a92dadf4d0eff62de842e098e5e02a
erwin <erwin.chang@titansoft.com.sg>
2020-01-04 12:10:09 +0800

    Move sha256Adapter to new file

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,31 +1,9 @@
 ﻿using System;
 using System.Net.Http;
-using System.Text;
 using SlackAPI;
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public class Sha256Adapter
-    {
-        public Sha256Adapter()
-        {
-        }
-
-        public string GetHashedPassword(string inputPwd)
-        {
-            var crypt = new System.Security.Cryptography.SHA256Managed();
-            var hash = new StringBuilder();
-            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(inputPwd));
-            foreach (var theByte in crypto)
-            {
-                hash.Append(theByte.ToString("x2"));
-            }
-
-            var hashedInputPWd = hash.ToString();
-            return hashedInputPWd;
-        }
-    }
-
     public class AuthenticationService
     {
         private ProfileDao _profileDao;
