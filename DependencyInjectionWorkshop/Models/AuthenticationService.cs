0c174b72b9f924c65f037ce6aaf263818d770ad6
erwin <erwin.chang@titansoft.com.sg>
2020-01-04 17:16:43 +0800

    Move Notification Decorator to another file

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,34 +1,5 @@
 ﻿namespace DependencyInjectionWorkshop.Models
 {
-    public class NotificationDecorator: IAuthenticationService
-    {
-        private readonly IAuthenticationService _authenticationService;
-        private readonly INotification _notification;
-
-
-        public NotificationDecorator(IAuthenticationService authenticationService, INotification notification)
-        {
-            _authenticationService = authenticationService;
-            _notification = notification;
-        }
-
-        private void Notify(string accountId)
-        {
-            _notification.Notify(accountId);
-        }
-
-        public bool Verify(string accountId, string inputPwd, string otp)
-        {
-            var isValid = _authenticationService.Verify(accountId, inputPwd, otp);
-            if (!isValid)
-            {
-                _notification.Notify(accountId);
-            }
-
-            return isValid;
-        }
-    }
-
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IProfile _profileDao;
