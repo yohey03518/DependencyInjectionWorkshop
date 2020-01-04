8f2530fc28b33cb8c72dbddf6979b0db2b267361
erwin <erwin.chang@titansoft.com.sg>
2020-01-04 14:49:29 +0800

    Add valid case unit test

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using DependencyInjectionWorkshop.Models;
+﻿using System;
+using DependencyInjectionWorkshop.Models;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -9,7 +11,19 @@ namespace DependencyInjectionWorkshopTests
         [Test]
         public void is_valid()
         {
-            var authenticationService = new AuthenticationService();
+            var fakeProfile = Substitute.For<IProfile>();
+            var fakeHash = Substitute.For<IHash>();
+            var fakeOtpService = Substitute.For<IOtpService>();
+            var fakeLogger = Substitute.For<ILogger>();
+            var fakeFailCounter = Substitute.For<IFailCounter>();
+            var fakeNotification = Substitute.For<INotification>();
+
+            var authenticationService = new AuthenticationService(fakeProfile, fakeHash, fakeOtpService, fakeFailCounter, fakeNotification, fakeLogger);
+            fakeProfile.GetPassword("erwin").Returns("hashedPwd");
+            fakeHash.Compute("pwd").Returns("hashedPwd");
+            fakeOtpService.GetCurrentOtp("erwin").Returns("1234");
+            var isValid = authenticationService.Verify("erwin", "pwd", "1234");
+            Assert.IsTrue(isValid);
         }
     }
 }
