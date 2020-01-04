0455859dc7e747831363463d4bc8379dc98e80ab
erwin <erwin.chang@titansoft.com.sg>
2020-01-04 12:27:29 +0800

    Move NlogAdapter to new file

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -3,19 +3,6 @@ using System.Net.Http;
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public class NLogAdapter
-    {
-        public NLogAdapter()
-        {
-        }
-
-        public void Info(string message)
-        {
-            var logger = NLog.LogManager.GetCurrentClassLogger();
-            logger.Info(message);
-        }
-    }
-
     public class AuthenticationService
     {
         private ProfileDao _profileDao;
---
+++ DependencyInjectionWorkshop/Models/NLogAdapter.cs
@@ -0,0 +1,15 @@
+﻿namespace DependencyInjectionWorkshop.Models
+{
+    public class NLogAdapter
+    {
+        public NLogAdapter()
+        {
+        }
+
+        public void Info(string message)
+        {
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Info(message);
+        }
+    }
+}
