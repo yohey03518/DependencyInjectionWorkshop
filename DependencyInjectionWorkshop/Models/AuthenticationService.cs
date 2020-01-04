a1a3abe8d8b873606a73c5c3ebcbddb181bddbe5
erwin <erwin.chang@titansoft.com.sg>
2020-01-04 10:09:57 +0800

    Add else to fit 91 code

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -49,15 +49,17 @@ namespace DependencyInjectionWorkshop.Models
                 resetResponse.EnsureSuccessStatusCode();
                 return true;
             }
+            else
+            {
+                var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
+                addFailedCountResponse.EnsureSuccessStatusCode();
 
+                string message = $"account: {accountId} try to login failed.";
+                var slackClient = new SlackClient("my api token");
+                slackClient.PostMessage(messageResponse => { }, "my channel", message, "my bot name");
+                return false;
+            }
 
-            var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
-            addFailedCountResponse.EnsureSuccessStatusCode();
-
-            string message = $"account: {accountId} try to login failed.";
-            var slackClient = new SlackClient("my api token");
-            slackClient.PostMessage(messageResponse => { }, "my channel", message, "my bot name");
-            return false;
         }
     }
 }
